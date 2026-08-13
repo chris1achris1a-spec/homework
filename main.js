b618282289e4c4c186d27e754da5a0c62f25b915
chris1achris1a-spec <chris1a.chris1a@gmail.com>
2026-08-13 13:21:08 +0000

    add db

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { input } from "@inquirer/prompts";
 import OpenAI from "openai";
 import { OPENAI_API_KEY } from "./config.js";
+import { initMessage, addMessage, getMessages } from "./db/messages.js";
 
 const client = new OpenAI({ apiKey: OPENAI_API_KEY });
 
@@ -43,4 +44,36 @@ while (true) {
     role: "assistant",
     content: answer,
   });
+}
+
+try {
+  while (true) {
+    const userQuestion = (
+      await input({ message: "請輸入你的問題：" })
+    ).trim();
+
+    if (userQuestion === "") continue;
+    if (userQuestion.toLowerCase() === "exit") {
+      console.log("再會~");
+      break;
+    }
+
+    await addMessage(userQuestion);
+
+    const response = await client.responses.create({
+      model: "gpt-5.6-luna",
+      input: getMessages(),
+    });
+
+    const content = response.output_text;
+    console.log(content);
+
+    await addMessage(content, "assistant");
+  }
+} catch (err) {
+  if (err.name === "ExitPromptError") {
+    console.log("\n再會~");
+  } else {
+    throw err;
+  }
 }
